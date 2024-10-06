1eb5e82a73daedb0282fe60247b749c1aa24e815
perdomo-c <luipotter@outlook.com>
2024-10-05 20:20:58 -0600

    week1

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Get the current year and set it in the footer
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get the last modified date and set it in the footer
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Updated: ${lastModified}`;
